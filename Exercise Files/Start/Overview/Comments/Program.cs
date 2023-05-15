d5339319628e18dd7b51b7b0d766709d206a39ba
jhan-dev <jhan.technology@gmail.com>
2023-05-15 12:29:24 -0700

    Comments, xml generation

--- Exercise Files/Start/Overview/Comments/Program.cs
+++ Exercise Files/Start/Overview/Comments/Program.cs
@@ -6,11 +6,21 @@ namespace Comments
     {
         /// XML Comments are used to help provide documentation
         /// They start with triple-slashes and have a special syntax
+        /// <summary>
+        /// This is the main sample application function
+        /// </summary>
+        /// <param name='args'>An array of string arguments from the command line</param>
+        /// <return>
+        /// No return value
+        /// </return>
         static void Main(string[] args)
         {
-
+            // Single line comments start with two clashes
+            // You can have many of these as you want
             Console.WriteLine("Hello World!");
-
+            /*
+            Multiple line comments start with a slash and a star and can continue for several lines until a closing star and slash are encountered. Best used for block commenting large sections over single line comments.
+            */
         }
     }
 }
