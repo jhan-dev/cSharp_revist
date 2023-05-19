870adb46bc8669b44843c8ff9c9b10bb6e9b6474
jhan-dev <jhan.technology@gmail.com>
2023-05-19 10:39:17 -0700

    Exceptions wiht try, catch and finally

--- Exercise Files/Start/Program Flow/Exceptions/Program.cs
+++ Exercise Files/Start/Program Flow/Exceptions/Program.cs
@@ -11,8 +11,24 @@ namespace Exceptions
             int result;
 
             // TODO: try-catch expressions make error checking easier
-            result = x / y;
-            Console.WriteLine("The result is: {0}", result);
+            try  {
+                if (x > 1000) {
+                    throw new ArgumentOutOfRangeException("x", "x has to be 1000 or less");
+                }
+                result = x / y;
+                Console.WriteLine("The result is: {0}", result);
+            }
+            catch (DivideByZeroException e) {
+                Console.WriteLine("There was an error!");
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e) {
+                Console.WriteLine("Sorry, 1000 is the limit");
+                Console.WriteLine(e.Message);
+            }
+            finally {
+                Console.WriteLine("This code will always run");
+            }
         }
     }
 }
